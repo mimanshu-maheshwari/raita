cf1f8d6b45685cba7c870111f585b5e65758a7a9
MIMANSHU MAHESHWARI <maheshwarimimanshu@gmail.com>
2025-03-13 18:27:47 +0530

    Moved stdin to separate thread
    Make a select type of option using thread To select between stdin and and interval of gossip message.

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,11 @@ mod unique_id;
 use init::InitPayload;
 use message::Message;
 use serde::{de::DeserializeOwned, Serialize};
-use std::io::{stdin, stdout, BufRead, BufReader, Write};
+use std::{
+    io::{stdin, stdout, BufRead, BufReader, Write},
+    sync::mpsc,
+    thread,
+};
 
 pub use broadcast::BroadcastPayload;
 pub use echo::EchoPayload;
@@ -25,29 +29,48 @@ where
 #[inline(always)]
 pub fn main_loop<Payload>(mut state: State) -> anyhow::Result<()>
 where
-    Payload: Sized + DeserializeOwned + Serialize,
+    Payload: Sized + DeserializeOwned + Serialize + Send + 'static,
     Message<Payload>: Node<Payload>,
 {
-    let stdin = stdin().lock();
+    let (tx, rx) = mpsc::channel();
+
     let mut stdout = stdout().lock();
 
+    let stdin = stdin().lock();
     let mut input_buffer = String::new();
     let mut reader = BufReader::new(stdin);
 
     reader.read_line(&mut input_buffer)?;
     let init_message: Message<InitPayload> = serde_json::from_str(&input_buffer)?;
     init_message.step(&mut stdout, &mut state)?;
-    drop(init_message);
 
-    input_buffer.clear();
-    while let Ok(bytes) = reader.read_line(&mut input_buffer) {
-        if bytes == 0 {
-            break;
-        }
-        let message: Message<Payload> = serde_json::from_str(&input_buffer)?;
-        message.step(&mut stdout, &mut state)?;
-        drop(message);
+    drop(init_message);
+    drop(input_buffer);
+    drop(reader);
+
+    // thread for stdin
+    let stdin_tx = tx.clone();
+    thread::spawn(move || {
+        let stdin = std::io::stdin().lock();
+        let mut input_buffer = String::new();
+        let mut reader = BufReader::new(stdin);
+
         input_buffer.clear();
+        while let Ok(bytes) = reader.read_line(&mut input_buffer) {
+            if bytes == 0 {
+                break;
+            }
+            let message: Message<Payload> = serde_json::from_str(&input_buffer)?;
+            if stdin_tx.send(message).is_err() {
+                return Ok::<_, anyhow::Error>(());
+            }
+            input_buffer.clear();
+        }
+        Ok::<(), anyhow::Error>(())
+    });
+
+    for message in rx {
+        message.step(&mut stdout, &mut state)?;
     }
     Ok(())
 }
